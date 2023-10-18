ecaec2cff6da2417991f312b4b248479da9972aa
pokemeta <pokemeta202@gmail.com>
2023-10-18 08:29:37 -0600

    Rough settings coded, only full screen for now

--- global.gd
+++ global.gd
@@ -1,3 +1,34 @@
 extends Node
 
 var pages_collected = 0
+
+var save_path = "./settings.save"
+
+var fullscreen = false
+
+func _ready():
+	load_settings()
+
+func save_settings():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(fullscreen)
+	apply_settings()
+
+func load_settings():
+	if FileAccess.file_exists(save_path):
+		var file = FileAccess.open(save_path, FileAccess.READ)
+		fullscreen = file.get_var(fullscreen)
+		apply_settings()
+	else:
+		create_settings_preferences()
+
+func create_settings_preferences():
+	var file = FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_var(fullscreen)
+	apply_settings()
+
+func apply_settings():
+	if fullscreen:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
--- mainmenu.gd
+++ mainmenu.gd
@@ -1,7 +1,27 @@
 extends Control
 
+@onready var main_container = $Main_container
+@onready var config_container = $Config_container
+
+@onready var fullscreen_check = $Config_container/Fullscreen_check
+
+func _ready():
+	fullscreen_check.button_pressed = Globals.fullscreen
+
 func _on_play_button_pressed():
 	get_tree().change_scene_to_file("res://world_navmeshtest.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+func _on_config_button_pressed():
+	main_container.visible = false
+	config_container.visible = true
+
+func _on_return_button_pressed():
+	main_container.visible = true
+	config_container.visible = false
+
+func _on_apply_button_pressed():
+	Globals.fullscreen = fullscreen_check.button_pressed
+	Globals.save_settings()
